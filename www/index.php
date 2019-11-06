3b69a32369ba79bceef4f34cf738b7332930791b
Antonina Solodskaya <mailstonya@gmail.com>
2019-11-06 12:55:22 +0200

    post new article

--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <title>Blog</title>
 </head>
@@ -12,46 +12,20 @@
     <?php require "blocks/header.php" ?>
     <div class="container mt-5">
         <h3 class="mb-5 d-flex justify-content-center">Our Articles</h3>
-        <div class="d-flex flex-column align-items-center">
+        <div class="d-flex flex-wrap">
+
         <?php
-            function printResult ($result_set, $result) {
-               
-                while (($row = $result_set->fetch_assoc()) != false && $row_image = mysql_fetch_array($result)) {
-                    $id = $row['id'];
-                    $title = $row['title'];
-                    $content = $row['content'];
-                    $date = $row['date'];
-                    
-                    echo 
-                    "<div class='card mb-4 w-75 shadow-sm'>
-                        <div class='card-header'>
-                            <h4 class='my-0 font-weight-normal'>$title</h4>
-                        </div>
-                        <img height='300' width='300' src='data:image;base64,'.$row_image[2].' '>
-                        <div class='card-body'>
-                            <p>$content</p>
-                            <p>$date</>
-                            <button type='button' class='btn btn-lg btn-block btn-outline-primary'><a href='view_post.php?pid=$id'>More details</a></button>
-                        </div>
-                    </div>";
-               
-                   //}
-                }
+            $db = mysqli_connect("localhost", "root", "", "mystore");
+            $result = mysqli_query($db, "SELECT * FROM `posts`");
+           
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<div class='card mb-4 shadow-sm'>";
+                echo "<div class='card-header'>","<h4 class='my-0 font-weight-normal'>" . $row['title'] . "</h4>","</div>";
+                echo "<img height='200' class='img-thumbnail' width='100%' src='images/" . $row['image'] . "' >";
+                echo "<div class='card-body'>", "<p>" . $row['content'] . "</p>", "<p>" . $row['date'] . "</p>", "<button type='button' class='btn btn-lg btn-block btn-outline-primary'>", "<a href='view_post.php?pid=$id'>" . More  . "</a>", "</button>", "</div>";
+                echo "</div>";
             }
-
-            $db = new mysqli ("localhost", "root", "", "mystore");
-            $db->query ("SET NAMES 'utf8'");
-            $result_set = $db->query ("SELECT * FROM `posts`");
-
-            $con = mysql_connect("localhost", "root", "");
-            mysql_select_db("mystore", $con);
-            $qry = "select * from images";
-            $result = mysql_query($qry, $con);
-
-            printResult ($result_set, $result);
-                    
-            $db->close();
-        ?>
+         ?>
         </div>
     </div>
     <?php require "blocks/footer.php" ?>
--- www/post.php
+++ www/post.php
@@ -1,56 +1,73 @@
 <?php
-    $errors = array('title' => '', 'content' => '');
+// $errors = array('title' => '', 'content' => '');
 
-    if(isset($_POST['submit'])) {
-        
-        if (getimagesize($_FILES['image']['tmp_name']) == FALSE) {
-            echo "Please select an image";
-        }else {
-            $image = addslashes($_FILES['image']['tmp_name']);
-            $name = addslashes($_FILES['image']['name']);
-            $image = file_get_contents($image);
-            $image = base64_encode($image);
-            saveImage($name, $image);
-        }
+// if(isset($_POST['submit'])) {
 
-        if(empty($_POST['title'])) {
-            $errors['title'] = "Required";  
-        }
+//     if(empty($_POST['title'])) {
+//         $errors['title'] = "Required";  
+//     }
 
-        if(empty($_POST['content'])) {
-            $errors['content'] = "Required";
+//     if(empty($_POST['content'])) {
+//         $errors['content'] = "Required";
+//     } else {
+
+//     $title = filter_var(trim($_POST['title']), FILTER_SANITIZE_STRING);
+//     $content = filter_var(trim($_POST['content']), FILTER_SANITIZE_STRING);
+
+//     $mysql = new mysqli("localhost", "root", "", "mystore");
+//     $mysql->query("SET NAMES 'utf8'");
+
+//     $mysql->query("INSERT INTO `posts` (`title`, `content`, `date`) VALUES ('$title', '$content', '".time ()."')");
+
+//     $mysql->close();
+
+//     header('Location: /');
+//     }
+// }
+
+?>
+<?php
+$errors = array('title' => '', 'content' => '', 'image' => '');
+$db = mysqli_connect("localhost", "root", "", "mystore");
+$msg = "";
+
+if (isset($_POST['submit'])) {
+    if (empty($_POST['title'])) {
+        $errors['title'] = "Required";
+    }
+
+    if (empty($_POST['image'])) {
+        $errors['image'] = "Required";
+    }
+
+    if (empty($_POST['content'])) {
+        $errors['content'] = "Required";
+    } else {
+        $image = $_FILES['image']['name'];
+        $content = mysqli_real_escape_string($db, $_POST['content']);
+        $title = mysqli_real_escape_string($db, $_POST['title']);
+        $target = "images/" . basename($image);
+
+        $sql = "INSERT INTO posts (title, image, content, date) VALUES ('$title', '$image', '$content', '" . time() . "')";
+
+        mysqli_query($db, $sql);
+
+        if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+            $msg = "Image uploaded successfully";
         } else {
+            $msg = "Failed to upload image";
+        }
 
-        $title = filter_var(trim($_POST['title']), FILTER_SANITIZE_STRING);
-        $content = filter_var(trim($_POST['content']), FILTER_SANITIZE_STRING);
-        
-        $mysql = new mysqli("localhost", "root", "", "mystore");
-        $mysql->query("SET NAMES 'utf8'");
-    
-        $mysql->query("INSERT INTO `posts` (`title`, `content`, `date`) VALUES ('$title', '$content', '".time ()."')");
-        
-        $mysql->close();
-    
+        $result = mysqli_query($db, "SELECT * FROM `posts`");
         header('Location: /');
-        }
     }
-  
-    function saveImage($name, $image) {
-        $con = mysql_connect("localhost", "root", "");
-        mysql_select_db("mystore", $con);
-        //$mysql = new mysqli("localhost", "root", "", "mystore");
-        $qry = "insert into images (name, image) VALUES ('$name', '$image')";
-        $result = mysql_query($qry, $con);
-        if ($result) {
-            //echo "<br/>Image uploaded";
-        } else {
-            echo "<br/>Image not uploaded";
-        }
-    }
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -59,27 +76,32 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <title>Blog</title>
 </head>
+
 <body>
     <?php require "blocks/header.php" ?>
-    <div class="container my-5 d-flex align-items-center flex-column">  
+    <div class="container my-5 d-flex align-items-center flex-column">
         <h1>Add Your Post</h1>
-        <form name="feedback" action="" method="post" class="w-100" enctype="multipart/form-data">
+        <form action="post.php" method="post" class="w-100" enctype="multipart/form-data">
             <div class="form-group">
                 <label for="title">Post Title</label>
                 <input type="text" name="title" class="form-control" id="title" placeholder="Write title">
-                <span style="color:red"><?php echo $errors['title'];?></span>
+                <span style="color:red"><?php echo $errors['title']; ?></span>
             </div>
             <div class="form-group">
                 <label for="content">Post Content</label>
                 <textarea name="content" class="form-control" id="content" placeholder="Write content" rows="3"></textarea>
-                <span style="color:red"><?php echo $errors['content'];?></span>
+                <span style="color:red"><?php echo $errors['content']; ?></span>
             </div>
-            <input type="file" name="image" >
-            <div class="d-flex justify-content-center">
-                <button type="submit" name="submit" value="submit" class="btn btn-success">Add Post</button>
+            <div class="form-group">
+                <label for="customFile">Choose image</label>
+                <input type="file" name="image" class="form-control-file" id="customFile">
+                <span style="color:red"><?php echo $errors['image']; ?></span>
             </div>
-        </form>
+            <button type="submit" name="submit" value="submit" class="btn btn-success mt-4">Add Post</button>
+    </div>
+    </form>
     </div>
     <?php require "blocks/footer.php" ?>
 </body>
+
 </html>
